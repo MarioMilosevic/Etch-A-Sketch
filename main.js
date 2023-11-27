21c06eb32ad00ca8973403c1f43056fccff8bcdd
Mario Milošević <mariomilosevic887@gmail.com>
2023-11-27 11:47:06 +0100

    Create layout

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
+"use strict";
 const root = document.querySelector("#app");
-const options = document.createElement("div");
-const main = document.createElement("div");
-function init() {
-  root.appendChild(options);
-  root.appendChild(main);
-}
-init();
+
+const displayGrid = (n) => {
+  for (let i = 0; i < n; i++) {
+    const div = document.createElement("div");
+    root.appendChild(div);
+    fillGrid(div, n);
+  }
+};
+
+const fillGrid = (element, n) => {
+  element.style.gridTemplate = `repeat(${n},1fr) / repeat(${n},1fr)`
+  element.style.backgroundColor = "#fff";
+  element.style.border = '1px solid black'
+};
+
+displayGrid(5);
