a16312d91263f3aebffe96b6421d06899aa02065
Mario Milošević <mariomilosevic887@gmail.com>
2023-11-27 10:39:03 +0100

    First commit

---
+++ main.js
@@ -0,0 +1,8 @@
+const root = document.querySelector("#app");
+const options = document.createElement("div");
+const main = document.createElement("div");
+function init() {
+  root.appendChild(options);
+  root.appendChild(main);
+}
+init();
