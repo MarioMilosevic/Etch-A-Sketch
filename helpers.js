e8efe0c917bd6f68ac7abcb3b95d15cceabf2f6e
Mario Milošević <mariomilosevic887@gmail.com>
2023-11-27 15:43:54 +0100

    Add options

--- helpers.js
+++ helpers.js
@@ -13,7 +13,6 @@ export const displayGrid = (root, n) => {
 const fillGrid = (main, divs, n) => {
   main.style.gridTemplate = `repeat(${n},1fr) / repeat(${n}, 1fr)`;
   divs.style.border = "1px solid black";
-  //   divs.style.backgroundColor = "#fff";
 };
 
 const fillDivs = () => {
