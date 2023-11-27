a231cb238d1a958f2f99a83faa5f1d0e79d0e74f
Mario Milošević <mariomilosevic887@gmail.com>
2023-11-27 22:28:28 +0100

    Toggle hover class to options

--- main.js
+++ main.js
@@ -1,14 +1,25 @@
 "use strict";
 import { displayGrid } from "./helpers";
 const app = document.querySelector("#app");
-const circleBtn = document.querySelector('#circleBtn')
-const colorModeBtn = document.querySelector('#colorModeBtn')
-const rainbowModeBtn = document.querySelector('#rainbowModeBtn')
-const eraserBtn = document.querySelector('#eraserBtn')
-const clearBtn = document.querySelector('#clearBtn')
-const numberRange = document.querySelector('.numberRange')
+const circleBtn = document.querySelector("#circleBtn");
+const colorModeBtn = document.querySelector("#colorModeBtn");
+const rainbowModeBtn = document.querySelector("#rainbowModeBtn");
+const eraserBtn = document.querySelector("#eraserBtn");
+const clearBtn = document.querySelector("#clearBtn");
+const numberRange = document.querySelector(".numberRange");
+
+const buttons = [circleBtn, colorModeBtn, rainbowModeBtn, eraserBtn, clearBtn];
+
+buttons.forEach((button) => {
+  button.addEventListener("mouseover", function () {
+    button.classList.add("hover");
+  });
+});
+
+buttons.forEach((button) =>
+  button.addEventListener("mouseleave", function () {
+    button.classList.remove("hover");
+  })
+);
 
 displayGrid(app, 8);
-
-
-
